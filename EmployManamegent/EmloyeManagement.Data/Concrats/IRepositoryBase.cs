0654add27c1e77299ecfbe2b1b8b6aa10fe51f06
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-06 01:21:30 +0300

    commit3

---
+++ EmployManamegent/EmloyeManagement.Data/Concrats/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EmloyeManagement.Data.Concrats
+{
+   public interface IRepositoryBase<T> where T:class,new()
+   {
+       IQueryable<T> GetAll(Expression<Func<T, bool>> filter= null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null, string include);
+   }
+}
