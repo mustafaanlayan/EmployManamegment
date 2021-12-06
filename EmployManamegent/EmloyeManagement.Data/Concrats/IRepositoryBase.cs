31ca46f21d22c9d86709fbe9aefeab25749ac72e
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-06 14:02:33 +0300

    commit repository

--- EmployManamegent/EmloyeManagement.Data/Concrats/IRepositoryBase.cs
+++ EmployManamegent/EmloyeManagement.Data/Concrats/IRepositoryBase.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace EmloyeManagement.Data.Concrats
 {
    public interface IRepositoryBase<T> where T:class,new()
    {
-       IQueryable<T> GetAll(Expression<Func<T, bool>> filter= null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null, string include);
+       IQueryable<T> GetAll(Expression<Func<T, bool>> filter= null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null, string includeProperties=null);
+      
+       T Get(int id);
+
+       T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string inculudeProperties = null);
+       void Add(T entity);
+       void Remove(T entity);
+       void Update(T entity);
    }
 }
---
+++ EmployManamegent/EmloyeManagement.Data/Implemention/Repository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using EmloyeManagement.Data.Concrats;
+using EmloyeManagement.Data.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmloyeManagement.Data.Implemention
+{
+    class Repository<T>:IRepositoryBase<T> where T:class ,new()
+    {
+        private readonly MustafaEmployeManamegentContext _ctx;
+        internal DbSet<T> dbSet;
+
+        public Repository(MustafaEmployeManamegentContext ctx)
+        {
+            _ctx = ctx;
+            this.dbSet = _ctx.Set<T>();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter!=null)
+            query = query.Where(filter);
+            if (includeProperties!=null)
+            {
+                foreach (var item in includeProperties.Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            if (orderBy!=null)
+            {
+                return orderBy(query);
+            }
+
+            return query;
+
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string inculudeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+            if (inculudeProperties != null)
+            {
+                foreach (var item in inculudeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+
+           return query.FirstOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Update(entity);
+        }
+    }
+}
